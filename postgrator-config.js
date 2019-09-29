10094033fec670624d6a922a78982b3de20d4024
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-09-29 16:40:54 -0400

    corrected code

--- postgrator-config.js
+++ postgrator-config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     "migrationsDirectory" : "migrations",
-    "client" : "pg",
+    "driver" : "pg",
     "connectionString" : (process.env.NODE === 'test')
         ? process.env.TEST_DB_URL
         : process.env.DB_URL
